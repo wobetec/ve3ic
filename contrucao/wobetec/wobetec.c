24c17e261a245cacb48bdaafbbe68ef270578e04
wobetec <esdras2821@gmail.com>
2021-06-08 17:53:49 -0300

    implementei o switch do passar

--- contrucao/wobetec/wobetec.c
+++ contrucao/wobetec/wobetec.c
@@ -34,9 +34,8 @@ typedef struct _opcao{
 
 typedef struct _no {
 	int indice;
-    int code; //7pppff (p=indice do pai, f="indice" do filho)
+    int code; //7pppttff (p=indice do pai, t=tipo, f="indice" do filho)
 	char texto[501];
-    no_complexo compl;
 	tipo_no tipo;
 	int n_opcoes;
 	opcao opcoes[MAX_OPCOES];
@@ -52,6 +51,8 @@ typedef struct _no {
 no *ptr_inicio, *ptr_atual;
 //Referencia para o arquivo de saida
 FILE *arquivo_saida;
+//referencia para a criação dos nós
+int contador_de_no = -1;
 
 //Criterios globais
 
@@ -60,7 +61,7 @@ FILE *arquivo_saida;
 char *ler_nome_jogador(char *nome_arquivo);
 
 //Gerenciamento da lista encadeada
-void cadastrar_no(int indice, int code, char **texto, int n_textos, tipo_no tipo, no_complexo compl, int n_opcoes, opcao *opcoes);
+void cadastrar_no(char **texto, int n_textos, tipo_no tipo, no_complexo compl, int n_opcoes, opcao *opcoes);
 no *buscar_no(int indice); //busa no na lista encadeada
 void apagar_lista();//freela alista encadeada
 
@@ -84,8 +85,12 @@ int main(){
     }
     
     //Página inicial com menuzinho
-    
+    char nome_jogador[100];
+    strcpy(nome_jogador, ler_nome_jogador(NOME_ARQUIVO_ENTRADA));
 
+
+    //Montagem da lista encadeada
+    cadastrar_nos();
 
 
 
@@ -94,7 +99,120 @@ int main(){
 
 ////////////////////////////OUTRAS FUNCOES////////////////////////////
 
+//faz a leitura do nome do jogador dentro do arquivo de entrada
 char *ler_nome_jogador(char *nome_arquivo){
-    
+    //variaveis
+    FILE *arquivo_entrada;
+    char *nome = NULL;
+    char buffer[100];
+
+    //abertura do arquivo
+    arquivo_entrada = fopen(nome_arquivo, "r");
+    if(arquivo_entrada == NULL){
+        printf("\nERRO 02: ERRO AO ABRIR O ARQUIVO DE ENTRADA.");
+        fprintf(arquivo_saida, "\nERRO 02: ERRO AO ABRIR O ARQUIVO DE ENTRADA.");
+        exit(1);
+    }
+
+    //copiando o arquivo para a variável nome
+    if(!feof(arquivo_entrada)){
+        nome = fgets(buffer, 100, arquivo_entrada);
+    }
+
+    //fechando o arquivo
+    fclose(arquivo_entrada);
+
+    //retorna a str nome
+    return nome;
 }
+
+
+void cadastrar_no(int code, char **texto, int n_textos, tipo_no tipo, no_complexo compl, int n_opcoes, opcao *opcoes){
+
+    //alocação dinâmica da memória
+    no *ptr = (no *) malloc(sizeof(no));
+    if(ptr == NULL){
+        printf("\nERRO 03: ERRO NA ALOCACAO DE MEMORIA.");
+        fprintf(arquivo_saida, "\nERRO 03: ERRO NA ALOCACAO DE MEMORIA.");
+        exit(1);
+    }
+
+    //swirch que define como serão os nós especiais criados
+    switch(compl){
+        
+        case passar:
+            //define o indice de forma automática
+            contador_de_no++;
+            ptr->indice = contador_de_no;
+            
+            //gerando o código daquele nó
+            int make = 70000000;// 7 000 00 00
+            if(code == -1){
+                make += (contador_de_no*10000);
+            }else{
+                make += ((contador_de_no-(code%100))*10000 + code);
+            }
+
+            ptr->code = make;
+            strcpy(ptr->texto, texto);
+            ptr->tipo = tipo;
+            ptr->n_opcoes = n_opcoes;
+
+            for(int i=0; i<n_opcoes; i++){
+                ptr->opcoes[i] = opcoes[i];
+            }
+
+            ptr->ant = NULL;
+            ptr->prox = NULL;
+
+            no *ptr_aux = ptr_inicio;
+            if(ptr_aux != NULL){
+                while(ptr_aux->prox != NULL){
+                    ptr_aux = ptr_aux->prox;
+                }
+                ptr_aux->prox = ptr;
+                ptr->ant =ptr_aux;
+            }else{
+                ptr_inicio = ptr;
+            }
+
+            break;
+        case entrada:
+             cadastrar_no(-1, texto[0], -1, tipo, passar, n_opcoes, opcoes);
+            break;
+        case sala_item:
+            
+            break;
+        case pedra:
+            
+            break;
+        case saida:
+            
+            break;
+        case fechadura:
+            
+            break;
+        case enigma:
+            
+            break;
+        case luta:
+            
+            break;
+        case nao_complexo:
+        
+            break;
+        default:
+            printf("\nERRO 04: TIPO DE NO INVALIDO.");
+            fprintf(arquivo_saida, "\nERRO 04: TIPO DE NO INVALIDO.");
+            exit(1);
+       }
+
+
+
+
+}
+
+
+void cadastrar_nos()
+
 
